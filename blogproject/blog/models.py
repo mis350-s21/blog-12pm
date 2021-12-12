3f366172f64486c27ab668b5fc8e28720cfda808
Mohamamd AlMarzouq <almarzouq@mis.cba.edu.kw>
2021-12-12 09:47:51 +0300

    save progress

--- blogproject/blog/models.py
+++ blogproject/blog/models.py
@@ -2,9 +2,18 @@ from django.db import models
 
 # Create your models here.
 class Post(models.Model):
-    title = models.CharField(max_length=100)
-    slug = models.SlugField(max_length=100)
+
+    STATUS = (
+        (0, "Draft"),
+        (1, "Published"),
+    )
+
+    title = models.CharField(max_length=100, unique=True)
+    slug = models.SlugField(max_length=100, unique=True)
     body = models.TextField(null=True, blank=True)
     created_at = models.DateTimeField(auto_now_add=True)
     updated_at = models.DateTimeField(auto_now=True)
-    status = models.IntegerField()
+    status = models.IntegerField(choices=STATUS, default=0)
+
+    def __str__(self):
+        return f"{self.title} created on {self.created_at}"
