32b78a406c100aaf2ee7ab3c4c0f28a9ddb6e7ba
Mohamamd AlMarzouq <almarzouq@mis.cba.edu.kw>
2021-12-23 10:00:53 +0300

    save progress

--- blogproject/blog/urls.py
+++ blogproject/blog/urls.py
@@ -6,5 +6,7 @@ urlpatterns = [
     path('', views.post_list, name='post_list'),
     path('published/', views.pub_post_list, name="pub_post_list"),
     path('unpublished/', views.draft_post_list, name="draft_post_list"),
+    path('post/<int:id>/', views.post_detail, name='post_detail'),
+    path('post/<slug:s>/', views.post_detail_slug, name='post_detail_slug'),
 ]
 
--- blogproject/blog/views.py
+++ blogproject/blog/views.py
@@ -18,3 +18,19 @@ def draft_post_list(request):
     posts = Post.objects.filter(status=0)
     c = {'post_list': posts,}
     return render(request, 'post_list.html', c)
+
+def post_detail(request, id):
+    p = Post.objects.get(id=id)
+    d = {
+        'post': p,
+    }
+    return render(request, 'post_detail.html', d)
+
+
+def post_detail_slug(request, s):
+    p = Post.objects.get(slug=s)
+    d = {
+        'post': p,
+    }
+    return render(request, 'post_detail.html', d)
+    
